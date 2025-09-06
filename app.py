5d3640d034df865b2b2c5612bb894f372315d255
Kloyzther <126795205+Kloyzther@users.noreply.github.com>
2025-09-06 00:02:47 -0500

    Update app.py
    puertos 0.0.0.0

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ if __name__ == "__main__":
     if not os.path.exists(DATES_FILE):
         with open(DATES_FILE, "w", encoding="utf-8") as f:
             json.dump({}, f)
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
 
